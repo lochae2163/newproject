ae6f18e59526d8a4af949b7a472eb95799cca322
LochanPun <Lochanpoon11@gmail.com>
2025-02-02 19:40:39 +0900

    setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+
+    '''
+    this function returns the list of requirements
+    ''' 
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    description='new ml project',
+    author='Lochan Pun',
+    author_email='lochanpoon11@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
